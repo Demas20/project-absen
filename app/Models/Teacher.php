997189487cedd9fa2436b22921879d66f55de78a
Demas20 <demasabiansya@gmail.com>
2025-03-03 10:32:01 +0700

    Nih lanjut

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Teacher extends Model
 {
     use HasFactory;
-    protected $table = 'teacher';
+    protected $table = 'teachers';
     protected $fillable = ['name', 'profile_pic'];
 }
